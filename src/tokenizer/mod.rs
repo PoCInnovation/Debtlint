fb41e5cff11355fa45a35f3d20a785ff9ccb9b90
enzo.ruffino <enzo.ruffino@epitech.eu>
2026-05-28 09:57:34 +0200

    [FIX] mod rs tokenizet

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -2,6 +2,7 @@ mod decode;
 mod pairs;
 mod replace;
 mod sequence;
+mod source;
 mod train;
 mod vocabulary;
 
@@ -9,5 +10,8 @@ pub use decode::{decode_sequence, decode_token};
 pub use pairs::{count_pairs, most_common_pair, TokenPair};
 pub use replace::replace_pair;
 pub use sequence::text_to_sequence;
+pub use source::SourceFile;
 pub use train::{train_bpe, BpeTrainingResult};
-pub use vocabulary::{Token, VocabularyEntry, BASE_VOCAB_SIZE};
+pub use vocabulary::{
+    FileId, FileOccurrences, Token, Vocabulary, VocabularyEntry, BASE_VOCAB_SIZE,
+};
