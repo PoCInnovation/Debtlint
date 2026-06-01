610175c23db31f6ac5960a4f3321aa8042e9abc5
enzo.ruffino <enzo.ruffino@epitech.eu>
2026-06-01 21:36:23 +0200

    [FIX] train with vector

--- src/tokenizer/train.rs
+++ src/tokenizer/train.rs
@@ -2,33 +2,35 @@ use std::collections::HashMap;
 
 use crate::tokenizer::pairs::{count_pairs, most_common_pair};
 use crate::tokenizer::replace::replace_pair;
-use crate::tokenizer::{Token, VocabularyEntry, BASE_VOCAB_SIZE};
+use crate::tokenizer::{Token, Vocabulary, BASE_VOCAB_SIZE};
 
 pub struct BpeTrainingResult { // struct to store the result of the training
     pub sequence: Vec<Token>, // sequence of token
-    pub vocabulary: HashMap<Token, VocabularyEntry>, // vocab of the training
+    pub vocabulary: Vocabulary, // vocab of the training
     pub merges: u32, // number of merges
 }
 
-pub fn train_bpe(mut sequence: Vec<Token>, target_vocab_size: u32,
-    min_frequency: usize) -> BpeTrainingResult { // recup the sequence, the target vocab size and the min frequency and return the struct
-    let mut vocabulary = HashMap::new();
-    let mut next_id = BASE_VOCAB_SIZE; // 256
+pub fn train_bpe( // recup the sequence the target vocab size and the min frequency occurrences
+    mut sequence: Vec<Token>,
+    target_vocab_size: u32, // nb of token in the vocab
+    min_frequency: usize, // min frequency of a pair to be merged
+) -> BpeTrainingResult {
+    let mut vocabulary = Vocabulary::init_base(); // init the vocab with the base letters
 
-    while next_id < target_vocab_size { // 256 -> 1000 max
+    while (vocabulary.len() as u32) < target_vocab_size { // while the vocab didnt reach the max size we fixe
         let counts: HashMap<(u32, u32), usize> = count_pairs(&sequence);
-        let Some((pair, frequency)) = most_common_pair(&counts) else { // return the pair with the highest frequency
-            break; // if no pair break loop
+        let Some((pair, frequency)) = most_common_pair(&counts) else { // return the pair with the highest frequence
+            break;
         };
         if frequency < min_frequency {
             break;
         }
-        vocabulary.insert(next_id, VocabularyEntry::merge(pair, vec![])); // merge the pair and vector of occurrences
-        sequence = replace_pair(&sequence, pair, next_id); // replace the pair with the new token
-        next_id += 1;
+        let new_token = vocabulary.push_merge(pair, vec![]); // merge the pair and vector of occurrences
+        sequence = replace_pair(&sequence, pair, new_token); // replace the pair with the new token
     }
+
     BpeTrainingResult {
-        merges: next_id - BASE_VOCAB_SIZE,
+        merges: vocabulary.len() as u32 - BASE_VOCAB_SIZE, // len - les 26
         sequence,
         vocabulary,
     }
