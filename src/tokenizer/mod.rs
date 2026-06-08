cbf09e3a702b8907f49d3d484452e5e7926eb733
enzo.ruffino <enzo.ruffino@epitech.eu>
2026-06-08 20:49:19 +0200

    [FIX] adpat mod

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -11,7 +11,7 @@ pub use pairs::{count_pairs, most_common_pair, TokenPair};
 pub use replace::replace_pair;
 pub use sequence::text_to_sequence;
 pub use source::SourceFile;
-pub use train::{train_bpe, BpeTrainingResult};
+pub use train::{train_bpe, train_corpus, BpeTrainingResult, FileTokens};
 pub use vocabulary::{
     FileOccurrences, Token, Vocabulary, VocabularyEntry, BASE_VOCAB_SIZE,
 };
