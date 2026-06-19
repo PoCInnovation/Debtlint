240efb13df9a13eea9d48eecec1f074b464c1818
enzo.ruffino <enzo.ruffino@epitech.eu>
2026-06-20 00:32:28 +0200

    [ADD] vocab in out, gestion js errors, save vocab in files and load it

--- src/in_out/mod.rs
+++ src/in_out/mod.rs
@@ -1,5 +1,7 @@
 mod reader;
+mod vocabulary_io;
 mod writer;
 
 pub use reader::read_corpus;
+pub use vocabulary_io::{load_vocabulary, save_vocabulary};
 pub use writer::write_encoded_sequence_json;
