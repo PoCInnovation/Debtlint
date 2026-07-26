6854720a4a6cce62dd98e01294796ad4e863cda0
rayan <rayan.ouerdane@epitech.eu>
2026-07-27 01:25:29 +0200

    fix: det-21 specify header in post request

--- action/src/api.js
+++ action/src/api.js
@@ -15,16 +15,17 @@ export async function createComment(octokit, diagnostic, context)
 {
     for (const range of diagnostic.ranges) {
         await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/comments', {
-          owner: context.repo.owner,
-          repo: context.repo.repo,
-          pull_number: context.payload.pull_request.number,
-          body: diagnostic.code_description,
-          commit_id: context.payload.pull_request.head.sha,
-          path: diagnostic.source,
-          start_line: range.start.line,
-          start_side: 'RIGHT',
-          line: range.end.line,
-          side: 'RIGHT',
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            pull_number: context.payload.pull_request.number,
+            body: diagnostic.code_description,
+            commit_id: context.payload.pull_request.head.sha,
+            path: diagnostic.source,
+            start_line: range.start.line,
+            start_side: 'RIGHT',
+            line: range.end.line,
+            side: 'RIGHT',
+            headers: { 'X-GitHub-Api-Version': '2026-03-10'}
         })
     }
 }
