fd253fe5d1a8a2d834c38d65f9d56a5fa8708f61
rayan <rayan.ouerdane@epitech.eu>
2026-07-27 01:10:23 +0200

    refactor: det-21 use octokit request directlyù

--- action/src/api.js
+++ action/src/api.js
@@ -13,25 +13,16 @@ import { diagnosticSchema } from './schemas.js';
  */
 export async function createComment(octokit, diagnostic, context)
 {
-    await octokit.issues.createComment({
-        ...context.repo,
-        issue_number: context.pull_request.number,
-        body: "Comment created."
+    await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/comments', {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.payload.pull_request.number,
+      body: diagnostic.code_description,
+      commit_id: context.payload.pull_request.head.sha,
+      path: diagnostic.source,
+      start_line: range.start.line,
+      start_side: 'RIGHT',
+      line: range.end.line,
+      side: 'RIGHT',
     })
 }
-
-//await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/comments', {
-//  owner: github.context.repo.owner,
-//  repo: github.context.repo.repo,
-//  pull_number: github.context.payload.pull_request.number,
-//  body: diagnostic.code_description,
-//  commit_id: github.context.payload.pull_request.head.sha,
-//  path: diagnostic.source,
-//  start_line: range.start.line,
-//  start_side: 'RIGHT',
-//  line: range.end.line,
-//  side: 'RIGHT',
-//  headers: {
-//    'X-GitHub-Api-Version': '2026-03-10',
-//  },
-//});
