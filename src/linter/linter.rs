4906cf403ea92dc9c8450f93ce0efbda68e28cb4
rayan <rayan.ouerdane@epitech.eu>
2026-07-27 02:14:57 +0200

    test: det-21 try with a file in the git diff

--- src/linter/linter.rs
+++ src/linter/linter.rs
@@ -6,12 +6,12 @@ pub fn get_duplicated() -> Diagnostic {
         severity: String::from("low"),
         ranges: vec![
             Range {
-                start: Position { source: "src/ingestion.rs".to_string(), line: 10, character: 1 },
-                end: Position { source: "src/ingestion.rs".to_string(), line: 26, character: 1 },
+                start: Position { source: "src/linter/linter.rs".to_string(), line: 10, character: 1 },
+                end: Position { source: "src/linter/linter.rs".to_string(), line: 19, character: 1 },
             },
             Range {
-                start: Position { source: "src/pipeline.rs".to_string(), line: 12, character: 1 },
-                end: Position { source: "src/pipeline.rs".to_string(), line: 23, character: 1 }
+                start: Position { source: "src/debug_run.rs".to_string(), line: 10, character: 1 },
+                end: Position { source: "src/debug_run.rs".to_string(), line: 16, character: 1 }
             }
         ],
         code_description: String::from("Duplicate"),
