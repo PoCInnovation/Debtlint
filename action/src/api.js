c0dd1d2b9cb48a7241e129616d1b9bcc622fcfdd
rayan <rayan.ouerdane@epitech.eu>
2026-08-04 01:36:54 +0200

    refactor: det-21 try to add markdown code to comment

--- action/src/api.js
+++ action/src/api.js
@@ -13,12 +13,25 @@ import { diagnosticSchema } from './schemas.js';
  */
 export async function createComment(octokit, diagnostic, context)
 {
+    let str = 
+        " <!-- cpp linter action --> " +
+        " ### clang-format suggestion " +
+        " ```suggestion " +
+        "         raylib::Vector3 alignToWorldPlane(raylib::Vector3 vector) const; " +
+        " ``` "
+        " ### clang-tidy diagnostics " +
+        " - function 'alignToWorldPlane' should be marked [[nodiscard]] [[modernize-use-nodiscard](https://clang.llvm.org/extra/clang-tidy/checks/modernize/use-nodiscard.html)]" +
+        " ```suggestion " +
+        "         [[nodiscard]] raylib::Vector3 alignToWorldPlane( " +
+        "             raylib::Vector3 vector) const; " + 
+        " ``` "
+
     for (const range of diagnostic.ranges) {
         await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
             owner: context.repo.owner,
             repo: context.repo.repo,
             issue_number: context.payload.pull_request.number,
-            body: diagnostic.code_description,
+            body: str,
             headers: { 'X-GitHub-Api-Version': '2026-03-10'}
         })
     }
