f66cce955a8633dcb029381433eec095e31f7d08
rayan <rayan.ouerdane@epitech.eu>
2026-08-07 23:33:30 +0200

    fix: format with rustfmt

--- src/linter/runner.rs
+++ src/linter/runner.rs
@@ -1,69 +1,69 @@
 use super::diagnostic::{Diagnostic, Position, Range};
 
-pub fn get_duplicated() ->  Vec<Diagnostic> {
+pub fn get_duplicated() -> Vec<Diagnostic> {
     vec![
-    Diagnostic {
-        code: 1,
-        severity: String::from("low"),
-        ranges: vec![
-            Range {
-                start: Position {
-                    source: "src/linter/runner.rs".to_string(),
-                    line: 10,
-                    character: 1,
+        Diagnostic {
+            code: 1,
+            severity: String::from("low"),
+            ranges: vec![
+                Range {
+                    start: Position {
+                        source: "src/linter/runner.rs".to_string(),
+                        line: 10,
+                        character: 1,
+                    },
+                    end: Position {
+                        source: "src/linter/runner.rs".to_string(),
+                        line: 19,
+                        character: 1,
+                    },
                 },
-                end: Position {
-                    source: "src/linter/runner.rs".to_string(),
-                    line: 19,
-                    character: 1,
+                Range {
+                    start: Position {
+                        source: "src/debug_run.rs".to_string(),
+                        line: 10,
+                        character: 1,
+                    },
+                    end: Position {
+                        source: "src/debug_run.rs".to_string(),
+                        line: 16,
+                        character: 1,
+                    },
                 },
-            },
-            Range {
-                start: Position {
-                    source: "src/debug_run.rs".to_string(),
-                    line: 10,
-                    character: 1,
+            ],
+            code_description: String::from("Duplicate"),
+        },
+        Diagnostic {
+            code: 2,
+            severity: String::from("high"),
+            ranges: vec![
+                Range {
+                    start: Position {
+                        source: "src/tokenizer/decode.rs".to_string(),
+                        line: 10,
+                        character: 1,
+                    },
+                    end: Position {
+                        source: "src/tokenizer/decode.rs".to_string(),
+                        line: 19,
+                        character: 1,
+                    },
                 },
-                end: Position {
-                    source: "src/debug_run.rs".to_string(),
-                    line: 16,
-                    character: 1,
+                Range {
+                    start: Position {
+                        source: "src/debug_run.rs".to_string(),
+                        line: 10,
+                        character: 1,
+                    },
+                    end: Position {
+                        source: "src/debug_run.rs".to_string(),
+                        line: 16,
+                        character: 1,
+                    },
                 },
-            },
-        ],
-        code_description: String::from("Duplicate"),
-    },
-    Diagnostic {
-        code: 2,
-        severity: String::from("high"),
-        ranges: vec![
-            Range {
-                start: Position {
-                    source: "src/tokenizer/decode.rs".to_string(),
-                    line: 10,
-                    character: 1,
-                },
-                end: Position {
-                    source: "src/tokenizer/decode.rs".to_string(),
-                    line: 19,
-                    character: 1,
-                },
-            },
-            Range {
-                start: Position {
-                    source: "src/debug_run.rs".to_string(),
-                    line: 10,
-                    character: 1,
-                },
-                end: Position {
-                    source: "src/debug_run.rs".to_string(),
-                    line: 16,
-                    character: 1,
-                },
-            },
-        ],
-        code_description: String::from("Duplicate code"),
-    }
+            ],
+            code_description: String::from("Duplicate code"),
+        },
     ]
 }
 
