7b2cc857ffa602ebe16b45d15e62f3002b339f03
rayan <rayan.ouerdane@epitech.eu>
2026-08-08 12:14:51 +0200

    test: det-21 call only linter in main(to test)

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,18 @@ use debtlint::pipeline::{BpeConfig, run_bpe};
 use ingestion::ingest_codebase;
 
 fn main() -> std::io::Result<()> {
-    let args = Args::parse();
-    let cfg = get_config();
-    let files = ingest_codebase(cfg);
-
-    let _result = run_bpe(
-        &files,
-        BpeConfig {
-            vocab_size: args.vocab_size,
-            min_frequency: args.min_frequency,
-        },
-        args.load_vocab.as_deref(),
-    )?;
+    //let args = Args::parse();
+    //let cfg = get_config();
+    //let files = ingest_codebase(cfg);
+//
+    //let _result = run_bpe(
+    //    &files,
+    //    BpeConfig {
+    //        vocab_size: args.vocab_size,
+    //        min_frequency: args.min_frequency,
+    //    },
+    //    args.load_vocab.as_deref(),
+    //)?;
     let _ = run_linter();
     Ok(())
 }
